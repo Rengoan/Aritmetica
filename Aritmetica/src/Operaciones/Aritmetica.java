4169ac0d82bca418fa7d752d4b27e915d183fa25
Rengoan <91058472+Rengoan@users.noreply.github.com>
2021-09-29 10:26:59 +0200

    aritmetica

--- Aritmetica/src/Operaciones/Aritmetica.java
+++ Aritmetica/src/Operaciones/Aritmetica.java
@@ -4,8 +4,31 @@ public class Aritmetica {
 
     int a, b;
 
+    //Constructor
+    public Aritmetica(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public Aritmetica() {
+    }
+    
+    //Metodos
+
     public void suma() {
-        
+
         System.out.println("La suma de " + a + "+" + b + " = " + (a + b));
+    }
+
+    public int suma2() {
+        return a + b;
+    }
+    
+    public int sumaArgs(int a, int b){
+        
+        this.a = a;
+        this.b = b;
+        return this.a + this.b;
+        
     }
 }
--- Aritmetica/src/Operaciones/Test.java
+++ Aritmetica/src/Operaciones/Test.java
@@ -3,11 +3,25 @@ package Operaciones;
 public class Test {
 
     public static void main(String[] args) {
-        
-        Aritmetica arit1 = new Aritmetica();
-        arit1.a = 5;
-        arit1.b = 7;
+
+//        Aritmetica arit1 = new Aritmetica();
+//        arit1.a = 5;
+//        arit1.b = 7;
+        Aritmetica arit1 = new Aritmetica(5, 7);
+        //1 caso
         arit1.suma();
+        //2 caso
+        var res = arit1.suma2();
+        System.out.println("Resultado caso 2 = " + res);
+        //3 caso
+        res = arit1.sumaArgs(8, 9);
+        System.out.println("Resultado caso 3 = " + res);
+        nuevoMetodo();
+
+    }
+
+    public static void nuevoMetodo() {
+        System.out.println("En el nuevo metodo");
 
     }
 
